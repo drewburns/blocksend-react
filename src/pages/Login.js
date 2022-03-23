ee7f4d2709c3e996b5a25a1377056d69af21c32f
drewburns <drewburnsbab@gmail.com>
2022-03-22 21:19:49 -0400

    added login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,98 @@
-import React from 'react'
-
+import React from "react";
+import { Card, Grid, Button, TextField } from "@mui/material";
+import axios from "axios";
+import { GlobalContext } from "../utility/GlobalContext";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  // const BASE_URL = "http://localhost:8080";
+  const BASE_URL = "https://fathomless-ravine-95441.herokuapp.com";
+  const [email, setEmail] = React.useState("");
+  const [verifyCode, setVerifyCode] = React.useState("");
+  const [codeSent, setCodeSent] = React.useState(false);
+  const [userId, setUserId] = React.useState(null);
+  const { state, setState } = React.useContext(GlobalContext);
+  let navigate = useNavigate();
+
+  const sendCode = () => {
+    setCodeSent(true);
+    axios
+      .post(BASE_URL + "/auth/code", { email })
+      .then((res) => {
+        setCodeSent(true);
+        setUserId(res.data.userId);
+      })
+      .catch((err) => {
+        alert("wrong email!");
+      });
+  };
+
+  const verifyLogin = () => {
+    axios
+      .post(BASE_URL + "/auth/login", { userId: userId, verifyCode })
+      .then((res) => {
+        setState({ jwt: res.data.token, user: res.data.user });
+        localStorage.setItem("id_token", res.data.token);
+        navigate("/wallet");
+      })
+      .catch((err) => {
+        alert("wrong code!");
+      });
+  };
   return (
-    <div>Login</div>
-  )
+    <Grid container>
+      <Grid item xs={3} />
+      <Grid item xs={6}>
+        <Card className="sendBlock">
+          <h3>Login</h3>
+          {codeSent ? (
+            <React.Fragment>
+              <div className="formBlock">
+                <TextField
+                  type="text"
+                  value={verifyCode}
+                  placeholder="123456"
+                  id="verifyCode"
+                  onChange={(e) => setVerifyCode(e.target.value)}
+                  label="Verify Code"
+                  variant="outlined"
+                />
+              </div>
+              <div className="formBlock">
+                <Button
+                  variant="contained"
+                  disabled={!verifyCode}
+                  onClick={verifyLogin}
+                >
+                  Verify
+                </Button>
+              </div>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <div className="formBlock">
+                <TextField
+                  type="email"
+                  value={email}
+                  placeholder="joe@smith.com"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Email"
+                  variant="outlined"
+                />
+              </div>
+              <div className="formBlock">
+                <Button
+                  variant="contained"
+                  disabled={!email}
+                  onClick={sendCode}
+                >
+                  Login
+                </Button>
+              </div>
+            </React.Fragment>
+          )}
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
