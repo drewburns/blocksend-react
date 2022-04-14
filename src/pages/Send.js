cc6dc896b1c990bcc6026a3b905123dcd06f9dd8
drewburns <drewburnsbab@gmail.com>
2022-04-14 00:30:08 -0400

    fixed host?

--- src/pages/Send.js
+++ src/pages/Send.js
@@ -8,7 +8,7 @@ import Checkout from "./Checkout";
 
 export default function Send(props) {
   // const BASE_URL = "http://localhost:8080";
-  const BASE_URL = "https://fathomless-ravine-95441.herokuapp.com";
+  const BASE_URL = "https://blocksend-dev.herokuapp.com";
   let navigate = useNavigate();
   const [amount, setAmount] = React.useState(25.0);
   const [email, setEmail] = React.useState("");
